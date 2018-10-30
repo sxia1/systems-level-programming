1c9660e906171205c129b5df56171716db8bb8f9
Sophia Xia <sxia1@stuy.edu>
2018-10-30 19:28:27 -0400

    I tried

--- stats.c
+++ stats.c
@@ -1,28 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 int main(){
  
     struct stat fileStat;
     if(stat("in.txt",&fileStat) < 0)    
         return 1;
- 
+    
     printf("Information for %s\n","in.txt");
     printf("---------------------------\n");
-    printf("File Size: \t\t%lld bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%llu\n",fileStat.st_ino);
- 
 
-    // int size = fileStat.st_mode;
-    // int size2 = size & 0b111111111;
-    // printf("file size originally: %o\n", size);
-    // printf("file size: %o\n",size2);
-
-
+    printf("File Permissions: \t%o\n",fileStat.st_mode & 0x1FF);
 
     printf("File Permissions: \t");
     printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
@@ -36,13 +28,37 @@ int main(){
     printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
     printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
 
-    // printf("\n%lu",sizeof(4));
-    //printf("\n%x\n", 100);
+    printf("\nNumber of Links: \t%ld\n",fileStat.st_nlink);
 
-    printf("\n%o\n",fileStat.st_mode & 0x1FF);
+    printf("User ID: \t\t%s\n", getpwuid(fileStat.st_uid) -> pw_name);
+    printf("Group ID: \t\t%s\n", getpwuid(fileStat.st_gid) -> pw_name);
 
-    printf("\n\n");
+    char *size;
+    char *format = "%ldB";
+    long file_size = fileStat.st_size;
+      
+    if(file_size/1000 > 0){
+      file_size /= 1000;
+      format = "%ldKB";
+    }
+    
+    if(file_size/1000 > 0){
+      file_size /= 1000;
+      format = "%ldMB";
+    }
+    
+    if(file_size/1000 > 0){
+      file_size /= 1000;
+      format = "%ldGB";
+    }
+    
+    sprintf(size, format, file_size);
+    
+    printf("File Size: \t\t%s\n", size);
+    
+    printf("Last Accessed: \t\t%s", ctime(&fileStat.st_atime));
 
- 
+    printf("\n");
+
     return 0;
 }
