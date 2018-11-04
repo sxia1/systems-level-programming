14c8ce58f0b4a2652776c253d51ebe0271e92d1c
Sophia Xia <sxia1@stuy.edu>
2018-11-04 18:04:55 -0500

    completion

--- dir_info.c
+++ dir_info.c
@@ -2,42 +2,36 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char *argv[]){
+  printf("Please enter the name of a directory: ");
+  char name[100];
+  scanf("%s", name);
   struct stat fileStat;
-  struct dirent entry;
+  struct dirent *entry;
   DIR *dir;
   
-  dir = opendir("test");
+  dir = opendir(name);
+
+  printf("Error: %s\n", strerror(errno));
+
+  if(strcmp(strerror(errno), "Success") == 0){
   
-  printf("Statistics for directory:\n");
+    printf("Statistics for directory:\n");
 
-  stat("test", &fileStat);
-  printf("Total Directory Size: %ldB\n", fileStat.st_size);
-
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-  entry = *readdir(dir);
-  printf("%s\n", entry.d_name);
-
-  /*
-    while(readdir(dir)){
-    printf("%s\n", entry.d_name);
-    entry = *readdir(dir);
+    stat("test", &fileStat);
+    printf("Total Directory Size: %ldB\n", fileStat.st_size);
+  
+    while(entry = readdir(dir)){
+      if(entry -> d_type == DT_DIR){
+	printf("%s\t-dir\n", entry -> d_name);
+      }
+      else{
+	printf("%s\t-file\n", entry -> d_name);
+      }
+    }
   }
-  */
-  
-  //printf("Directories:");
-
-  //printf("Regular files:");
-  
   closedir(dir);
 }
