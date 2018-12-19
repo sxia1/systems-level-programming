ea20df236c24424d06ed0f04c7285cad3dbb8f33
sxia1 <sxia1@stuy.edu>
2018-12-19 15:25:42 -0500

    correction

--- pipe_networking.c
+++ pipe_networking.c
@@ -14,27 +14,29 @@ int server_handshake(int *to_client) {
 
   //server creates the well known pipe
   printf("creating WKP\n");
-  int WKP = mkfifo("WELL_KNOWN_PIPE", 0666);
+  //don't need int WKP = b/c it doesn't return a file descriptor
+  mkfifo("WELL_KNOWN_PIPE", 0666);
   printf("1. create: %s\n", strerror(errno));
 
   //server receives the message
   int receive = open("WELL_KNOWN_PIPE", O_RDONLY);
-  printf("4. receive: %s\n", strerror(errno));
+  printf("4. opening read end: %s\n", strerror(errno));
   char PP[BUFFER_SIZE];
-  int reading = read(receive, PP, BUFFER_SIZE);
+  read(receive, PP, BUFFER_SIZE); //don't need int reading =
   printf("5. read: %s\n", strerror(errno));
 
   //server removes the well known pipe
+  printf("removing WKP\n");
   remove("WELL_KNOWN_PIPE");
 
   //server send message to client
   *to_client = open(PP, O_WRONLY);
-  printf("6. opening: %s\n", strerror(errno)); 
-  int writing = write(*to_client, ACK ,sizeof(ACK));
+  printf("6. opening write end: %s\n", strerror(errno)); 
+  write(*to_client, ACK ,sizeof(ACK));
   printf("7. writing: %s\n", strerror(errno));
 
   //server receives a response
-  reading = read(receive, PP, BUFFER_SIZE);
+  read(receive, PP, BUFFER_SIZE);
   printf("10. read: %s\n", strerror(errno));
   printf("Handshake Complete");
   return receive;
@@ -57,27 +59,32 @@ int client_handshake(int *to_server) {
 
   //client create the private pipe
   printf("creating PP\n");
-  int PP = mkfifo(PRIVATE_PIPE, 0666);
+  mkfifo(PRIVATE_PIPE, 0666);
   printf("2. create: %s\n", strerror(errno));
 
   //client connects to the server
   *to_server = open("WELL_KNOWN_PIPE", O_WRONLY);
+  printf("opening write end: %s\n", strerror(errno));
 
   //client sends a message to the server
-  int writing = write(*to_server, PRIVATE_PIPE, BUFFER_SIZE);
+  write(*to_server, PRIVATE_PIPE, BUFFER_SIZE);
   printf("3. write: %s\n", strerror(errno));
-  
+
   //client receives message
-  int receive = open("PRIVATE_PIPE", O_RDONLY);
+  //OPEN PRIVATE_PIPE NOT "PRIVATE_PIPE"
+  int receive = open(PRIVATE_PIPE, O_RDONLY);
+  printf("opening read end: %s\n", strerror(errno));
   char mssg[BUFFER_SIZE];
-  int reading = read(receive, mssg, BUFFER_SIZE);
+  read(receive, mssg, BUFFER_SIZE);
   printf("8. read: %s\n", strerror(errno));
 
   //client removes private pipe
-  remove("PRIVATE_PIPE");
-  printf("9. write: %s\n", strerror(errno));
+  //REMOVE PRIVATE_PIPE NOT "PRIVATE_PIPE"
+  remove(PRIVATE_PIPE);
+  printf("removing PP");
 
   //client responds to the message
-  writing = write(*to_server, ACK, sizeof(ACK));
+  write(*to_server, ACK, sizeof(ACK));
+  printf("9. write: %s\n", strerror(errno));
   return receive;
 }
